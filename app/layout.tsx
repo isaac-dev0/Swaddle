b2e32657763dddef90f96069407f812cf359dbc9
Isaac D <isaac_dolphin@outlook.com>
2026-01-08 18:09:44 +0000

    feat(Implemented Supabase configuration)

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex min-h-screen items-center justify-center">
+          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
